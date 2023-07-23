9c2d0338fbd15a19ffce98ecdbd4238bd504db5b
Craig Pretorius <craigp@codehq.co.za>
2023-07-23 06:12:54 +0000

    GitHub Issues widget

---
+++ src/views/dashboard/DashboardGitHubIssues.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card>
+    <v-card-title class="align-start">
+      <span class="font-weight-semibold">GitHub Issues</span>
+      <v-spacer></v-spacer>
+      <v-btn
+        icon
+        small
+        class="me-n3 mt-n2"
+      >
+        <v-icon>
+          {{ mdiDotsVertical }}
+        </v-icon>
+      </v-btn>
+    </v-card-title>
+
+    <v-card-subtitle class="mb-8 mt-n5">
+      <span class="font-weight-semibold text--primary me-1">Total {{ total }} issues</span>
+      <span>😎</span>
+    </v-card-subtitle>
+
+    <v-card-text>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup>
+// eslint-disable-next-line object-curly-newline
+import { mdiDotsVertical } from '@mdi/js';
+
+</script>
